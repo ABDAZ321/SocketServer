44d034a55e4d1a2c2d3de6f4ac64596d956cb030
Abdullah Azeem <aa6367@sd62.bc.ca>
2023-02-27 11:26:14 -0800

    reval the goat

--- constant.js
+++ constant.js
@@ -1,8 +1,8 @@
 //Sets the amount of times screen updates every 1000 milliseconds
-const FRAME_RATE = 8;
+const FRAME_RATE = 10;
 
 //Sets the amount of gridsizes avalible on gride space
-const GRID_SIZE = 60;
+const GRID_SIZE = 50;
 
 module.exports = {
     FRAME_RATE,
