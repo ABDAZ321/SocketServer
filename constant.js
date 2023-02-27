a73f9ca2e62a27da4c1d91d71c11dabd6fa79ba8
ABDAZ321 <36831803+ABDAZ321@users.noreply.github.com>
2023-02-27 12:54:30 -0800

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,8 +1,8 @@
 //Sets the amount of times screen updates every 1000 milliseconds
-const FRAME_RATE = 10;
+const FRAME_RATE = 7;
 
 //Sets the amount of gridsizes avalible on gride space
-const GRID_SIZE = 50;
+const GRID_SIZE = 25;
 
 module.exports = {
     FRAME_RATE,
